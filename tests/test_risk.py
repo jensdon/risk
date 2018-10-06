029095526baa6e23bb315b5bf34ffb1a7eae72b8
Jelle Pals <jellepals@Jelles-MacBook-Pro.local>
2018-10-06 13:54:05 +0200

    Created player generator

--- tests/test_risk.py
+++ tests/test_risk.py
@@ -6,14 +6,17 @@ class RiskTest(unittest.TestCase):
     def setUp(self):
         self.newGame = Risk()
 
+    def generateRandomplayers(self,amount):
+        for color in range(amount):
+            self.newGame.addplayer("Player", str(color), True);
+
     def test_addplayer(self):
-        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.generateRandomplayers(1);
         getplayercount = len(self.newGame.getplayers());
         self.assertEqual(1, getplayercount, "Player is not found in game.")
 
     def test_addtomuchplayers(self):
-        for color in range(6):
-            self.newGame.addplayer("Player", str(color), True);
+        self.generateRandomplayers(6);
         with self.assertRaises(TooManyPlayersException):
           self.newGame.addplayer("PlayerOne", "blue", True)
 
