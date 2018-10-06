c9f229e4bc1236cd6f94acb8b8bad720ca18932f
Jens Don <jensdon@gmail.com>
2018-10-06 11:28:01 +0200

    Create tests for to many players

--- tests/test_risk.py
+++ tests/test_risk.py
@@ -12,6 +12,15 @@ class RiskTest(unittest.TestCase):
         getplayercount = len(self.newGame.getplayers());
         self.assertEqual(1, getplayercount, "Player is not found in game.")
 
+    def test_addtomuchplayers(self):
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.newGame.addplayer("PlayerOne", "blue", True);
+        self.assertRaises(TooManyPlayersError, self.newGame.addplayer("PlayerOne", "blue", True))
+
 
 if __name__ == '__main__':
     unittest.main()
