065c5020b84a94d1c8aa8ecd1c1c3bc87a7d34b4
Jens Don <jensdon@gmail.com>
2018-10-13 13:17:23 +0200

    Create test for creating gamemap

--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -1,5 +1,6 @@
 import unittest
 from factories.GameMapFactory import GameMapFactory, GameMapNotExists
+from entities.GameMap import GameMap
 
 
 class GameMapTest(unittest.TestCase):
@@ -8,6 +9,9 @@ class GameMapTest(unittest.TestCase):
         with self.assertRaises(GameMapNotExists):
             self.game_map = GameMapFactory.make_game_map("not_existing_map")
 
+    def test_check_if_game_map_created(self):
+        game_map = GameMapFactory.make_game_map("default")
+        self.assertIsInstance(game_map, GameMap)
 
 if __name__ == '__main__':
     unittest.main()
