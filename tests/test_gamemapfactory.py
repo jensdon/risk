9e02d5bcef16fbabc93ee5f4b83472e23bf26271
Jens Don <jensdon@gmail.com>
2018-10-27 11:45:43 +0200

    Create test for continents

--- tests/test_gamemapfactory.py
+++ tests/test_gamemapfactory.py
@@ -12,6 +12,11 @@ class GameMapTest(unittest.TestCase):
     def test_can_create_gamemap(self):
         self.assertTrue(type(GameMapFactory.load_game_map("default")) is GameMap)
 
+    def test_gamemap_has_continents(self):
+        game_map = GameMapFactory.load_game_map("default")
+        continents = game_map.get_continents()
+        self.assertTrue(len(continents) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
