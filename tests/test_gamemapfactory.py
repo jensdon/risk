3a15dff8125a8c9e81c6bd6f490b5e5af68e9c80
Jelle Pals <jellepals@Jelles-MacBook-Pro.local>
2018-10-27 12:18:30 +0200

    Refactor: make tests more efficient

--- tests/test_gamemapfactory.py
+++ tests/test_gamemapfactory.py
@@ -5,6 +5,10 @@ from entities.GameMap import GameMap
 
 class GameMapTest(unittest.TestCase):
 
+    def generate_normal_map(self):
+        game_map = GameMapFactory.load_game_map("default")
+        self.continents = game_map.get_continents()
+
     def test_check_if_map_not_exist(self):
         with self.assertRaises(GameMapNotExists):
             self.game_map = GameMapFactory.load_game_map("not_existing_map")
@@ -13,14 +17,12 @@ class GameMapTest(unittest.TestCase):
         self.assertTrue(type(GameMapFactory.load_game_map("default")) is GameMap)
 
     def test_gamemap_has_continents(self):
-        game_map = GameMapFactory.load_game_map("default")
-        continents = game_map.get_continents()
-        self.assertTrue(len(continents) > 0)
+        self.generate_normal_map()
+        self.assertTrue(len(self.continents) > 0)
 
     def test_gamemap_continents_have_territories(self):
-        game_map = GameMapFactory.load_game_map("default")
-        continents = game_map.get_continents()
-        for continent in continents:
+        self.generate_normal_map()
+        for continent in self.continents:
             self.assertTrue(len(continent.get_territories()) > 0)
 
 
