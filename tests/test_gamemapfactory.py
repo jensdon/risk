c3f9418a233b30a1fbfc163832e268a1142f25e6
Jens Don <jensdon@gmail.com>
2018-10-27 11:09:14 +0200

    Create test for creating gamemap

--- tests/test_gamemapfactory.py
+++ tests/test_gamemapfactory.py
@@ -1,12 +1,15 @@
 import unittest
 from factories.GameMapFactory import GameMapFactory, GameMapNotExists
 
+
 class GameMapTest(unittest.TestCase):
 
     def test_check_if_map_not_exist(self):
         with self.assertRaises(GameMapNotExists):
             self.game_map = GameMapFactory.load_game_map("not_existing_map")
 
+    def test_can_create_gamemap(self):
+        self.assertTrue(type(GameMapFactory.load_game_map("default")) is GameMap)
 
 if __name__ == '__main__':
     unittest.main()
